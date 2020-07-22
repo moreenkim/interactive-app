47ecd407a450b9572f5d06fa78ae3a7ed98cd42f
Moreen <moreenkim36@gmail.com>
2020-07-22 12:02:59 +0300

    Add home page html boilerplate code

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
   res.send('welcome to our new app');
 });
