fbe6b115c7da164a0668897aa25d52df8d303c72
Moreen <moreenkim36@gmail.com>
2020-07-22 12:09:43 +0300

    Install .ejs template language and add css boilerplate

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('welcome to our new app');
+  res.render('home-guest');
 });
 
 app.listen(3001);
