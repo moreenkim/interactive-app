7617611a3dcab8a09ed836d39a9b24c427f895be
Moreen <moreenkim36@gmail.com>
2020-07-23 11:05:32 +0300

    Add more validation measures

--- models/User.js
+++ models/User.js
@@ -5,6 +5,25 @@ let User = function (data) {
   this.errors = [];
 };
 
+User.prototype.cleanUp = function () {
+  if (typeof this.data.username !== 'string') {
+    this.data.username = '';
+  }
+  if (typeof this.data.email !== 'string') {
+    this.data.email = '';
+  }
+  if (typeof this.data.password !== 'string') {
+    this.data.password = '';
+  }
+
+  //get rid of bogus properties
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password,
+  };
+};
+
 User.prototype.validate = function () {
   if (this.data.username === '') {
     this.errors.push('You must provide a username');
@@ -37,6 +56,7 @@ User.prototype.validate = function () {
 
 User.prototype.register = function () {
   //step1 validate user data
+  this.cleanUp();
   this.validate();
 
   //step2 if theres no validation errors, save user data to db
