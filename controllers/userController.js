1a55a4abb84c472aa2060509152684511464bf5c
Moreen <moreenkim36@gmail.com>
2020-07-24 15:57:58 +0300

    Enable create post functionality while refactoring reusable code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('../models/User');
 
+exports.mustBeLoggedIn = function (req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash('errors', 'you must be logged in');
+    req.session.save(function () {
+      res.redirect('/');
+    });
+  }
+};
+
 exports.login = function (req, res) {
   let user = new User(req.body);
   user
@@ -46,10 +57,7 @@ exports.register = function (req, res) {
 
 exports.home = function (req, res) {
   if (req.session.user) {
-    res.render('home-dashboard', {
-      username: req.session.user.username,
-      avatar: req.session.user.avatar,
-    });
+    res.render('home-dashboard');
   } else {
     res.render('home-guest', {
       errors: req.flash('errors'),
