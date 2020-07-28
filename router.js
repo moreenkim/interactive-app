079cccc545825be3ec3b466bca01df2bbecb3c9c
Moreen <moreenkim36@gmail.com>
2020-07-28 11:36:39 +0300

    Let users follow each otherand store this in the db

--- router.js
+++ router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('./controllers/userController');
 const postController = require('./controllers/postController');
+const followController = require('./controllers/followController');
 
 // user related routes
 router.get('/', userController.home);
@@ -44,5 +45,12 @@ router.post(
   postController.delete
 );
 router.post('/search', postController.search);
+
+//follow related routes
+router.post(
+  '/addFollow/:username',
+  userController.mustBeLoggedIn,
+  followController.addFollow
+);
 
 module.exports = router;
