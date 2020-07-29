69810ca9d4ade30086cd23d9210a47285f6c29b5
Moreen <moreenkim36@gmail.com>
2020-07-29 12:15:47 +0300

    Adjust our app to use csrf

--- frontend-js/modules/registrationForm.js
+++ frontend-js/modules/registrationForm.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export default class RegistrationForm {
   constructor() {
+    this._csrf = document.querySelector('[name="_csrf"]').value;
     this.form = document.querySelector('#registration-form');
     this.allFields = document.querySelectorAll(
       '#registration-form .form-control'
@@ -24,26 +25,22 @@ export default class RegistrationForm {
       e.preventDefault();
       this.formSubmitHandler();
     });
+
     this.username.addEventListener('keyup', () => {
       this.isDifferent(this.username, this.usernameHandler);
     });
-
     this.email.addEventListener('keyup', () => {
       this.isDifferent(this.email, this.emailHandler);
     });
-
     this.password.addEventListener('keyup', () => {
       this.isDifferent(this.password, this.passwordHandler);
     });
-
     this.username.addEventListener('blur', () => {
       this.isDifferent(this.username, this.usernameHandler);
     });
-
     this.email.addEventListener('blur', () => {
       this.isDifferent(this.email, this.emailHandler);
     });
-
     this.password.addEventListener('blur', () => {
       this.isDifferent(this.password, this.passwordHandler);
     });
@@ -82,12 +79,6 @@ export default class RegistrationForm {
     this.username.timer = setTimeout(() => this.usernameAfterDelay(), 800);
   }
 
-  emailHandler() {
-    this.email.errors = false;
-    clearTimeout(this.email.timer);
-    this.email.timer = setTimeout(() => this.emailAfterDelay(), 800);
-  }
-
   passwordHandler() {
     this.password.errors = false;
     this.passwordImmediately();
@@ -99,7 +90,7 @@ export default class RegistrationForm {
     if (this.password.value.length > 50) {
       this.showValidationError(
         this.password,
-        'password cannot exceed 50 characters'
+        'Password cannot exceed 50 characters.'
       );
     }
 
@@ -112,28 +103,34 @@ export default class RegistrationForm {
     if (this.password.value.length < 12) {
       this.showValidationError(
         this.password,
-        'password must be atleast 12 characters'
+        'Password must be at least 12 characters.'
       );
     }
+  }
+
+  emailHandler() {
+    this.email.errors = false;
+    clearTimeout(this.email.timer);
+    this.email.timer = setTimeout(() => this.emailAfterDelay(), 800);
   }
 
   emailAfterDelay() {
     if (!/^\S+@\S+$/.test(this.email.value)) {
       this.showValidationError(
         this.email,
-        'you must provide a valid email address'
+        'You must provide a valid email address.'
       );
     }
 
     if (!this.email.errors) {
       axios
-        .post('/doesEmailExist', { email: this.email.value })
+        .post('/doesEmailExist', { _csrf: this._csrf, email: this.email.value })
         .then((response) => {
           if (response.data) {
             this.email.isUnique = false;
             this.showValidationError(
               this.email,
-              'that email is already in use'
+              'That email is already being used.'
             );
           } else {
             this.email.isUnique = true;
@@ -141,7 +138,7 @@ export default class RegistrationForm {
           }
         })
         .catch(() => {
-          console.log('try again later');
+          console.log('Please try again later.');
         });
     }
   }
@@ -189,12 +186,15 @@ export default class RegistrationForm {
 
     if (!this.username.errors) {
       axios
-        .post('/doesUsernameExist', { username: this.username.value })
+        .post('/doesUsernameExist', {
+          _csrf: this._csrf,
+          username: this.username.value,
+        })
         .then((response) => {
           if (response.data) {
             this.showValidationError(
               this.username,
-              'that username is already taken'
+              'That username is already taken.'
             );
             this.username.isUnique = false;
           } else {
@@ -202,7 +202,7 @@ export default class RegistrationForm {
           }
         })
         .catch(() => {
-          console.log('please try again later');
+          console.log('Please try again later.');
         });
     }
   }
